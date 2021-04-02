d9511aab55e6415e407fbd829ce99c6f3b57dfd8
Nick Henry <iisevensii@gmail.com>
2021-04-02 17:46:32 -0500

    Comments explaining current segment logic

--- Infrastructure/Snake.cs
+++ Infrastructure/Snake.cs
@@ -14,6 +14,7 @@ namespace MonoSnake.Infrastructure
         private readonly List<SnakeSegment> _snakeSegments = new List<SnakeSegment>();
         private int _segmentCount = 1;
         private Sprite _snakeTailSprite;
+        Vector2 _lastSegmentPosition = Vector2.Zero;
 
         public Snake(SnakeHead snakeHead, Sprite snakeTailSprite)
         {
@@ -42,37 +43,50 @@ namespace MonoSnake.Infrastructure
             if(!_snakeHead.CanUpdate())
                 return;
 
-            Vector2 previousSnakeHeadPosition = _snakeHead.Position;
             // Move Snake Head
             _snakeHead.Update(gameTime);
+
+            // Previous position of the SnakeHead
+            Vector2 previousSnakeHeadPosition = _snakeHead.Position;
+
+            // For tracking the previous segment's position and applying to the current segment
             Vector2 previousSegmentPosition = Vector2.Zero;
+
             // Move Segments
             foreach (SnakeSegment snakeSegment in _snakeSegments)
             {
                 int currentIndex = _snakeSegments.IndexOf(snakeSegment);
 
+                // Neck (First Segment - Follows SnakeHead directly)
                 if (currentIndex == 0)
                 {
                     snakeSegment.PreviousSnakeSegmentPosition = previousSnakeHeadPosition;
                     previousSegmentPosition = snakeSegment.PreviousSnakeSegmentPosition;
                 }
+                // All other segments follow the previous segment
                 else
                 {
-                    var newPosition = previousSegmentPosition;
+                    // Position to be applied to the current segment
+                    Vector2 newPosition = previousSegmentPosition;
+                    // Position to be applied to the next segment (if it exists)
                     previousSegmentPosition = snakeSegment.Position;
+                    // Set current segment position to that of the last segment
                     snakeSegment.PreviousSnakeSegmentPosition = newPosition;
                 }
 
+                // Apply Correct Sprite and Rotation based on the direction the current segment is heading
                 snakeSegment.Sprite.Rotation = (float)(90 * Math.PI / 180);
                 snakeSegment.Update(gameTime);
             }
 
+            // Save last segment position so that new segments can use it as their starting position
             _lastSegmentPosition = _snakeSegments.Last().Position;
             
+            // Temporary Code for testing
             if(_segmentCount < 9)
                 _segmentCount++;
         }
-        Vector2 _lastSegmentPosition = Vector2.Zero;
+
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             _snakeHead.Draw(spriteBatch, gameTime);
