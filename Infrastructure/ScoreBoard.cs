48f6de9f2bede5071b62f83df9c6a17a4b5afa2e
Nick Henry <iisevensii@gmail.com>
2021-04-12 11:18:11 -0500

    ScoreBoard - Only record new high scores

--- Infrastructure/ScoreBoard.cs
+++ Infrastructure/ScoreBoard.cs
@@ -12,6 +12,8 @@ namespace MonoSnake.Infrastructure
         private const string HIGH_SCORE_FILE_NAME = "HighScores.json";
         private string _applicationPath;
         private string _highScoreStoragePath;
+        private JsonSerializerOptions _jsonSerializerOptions;
+
         public int AllTimeHighScore { get; set; }
         public int CurrentScore { get; set; }
         public HighScores HighScores { get; set; }
@@ -21,6 +23,8 @@ namespace MonoSnake.Infrastructure
             _applicationPath = applicationPath;
             _highScoreStoragePath = Path.Combine(Path.GetDirectoryName(_applicationPath), HIGH_SCORE_FILE_NAME);
             HighScores = LoadHighScores();
+            _jsonSerializerOptions = new JsonSerializerOptions() {WriteIndented = true};
+            
         }
 
         public HighScores LoadHighScores()
@@ -37,15 +41,21 @@ namespace MonoSnake.Infrastructure
 
         public void SaveHighScores()
         {
-            string highScoresSerialized = JsonSerializer.Serialize(HighScores);
+            if (HighScores.HighScoresUpdated)
+            {
+                string highScoresSerialized = JsonSerializer.Serialize(HighScores, _jsonSerializerOptions);
 
 
-            File.WriteAllText(_highScoreStoragePath, highScoresSerialized);
+                File.WriteAllText(_highScoreStoragePath, highScoresSerialized);
+
+                HighScores.HighScoresUpdated = false;
+            }
         }
     }
 
     public class HighScores
     {
+        public bool HighScoresUpdated { get; set; }
         public List<ScoreEntry> ScoreEntries { get; set; } = new List<ScoreEntry>();
 
         private IEnumerable<ScoreEntry> Top10OrderedScoreEntries()
@@ -55,10 +65,11 @@ namespace MonoSnake.Infrastructure
 
         public void AddHighScore(ScoreEntry scoreEntry)
         {
-            if (!ScoreEntries.Any() || ScoreEntries.Any(se => scoreEntry.Score > se.Score))
+            if (!ScoreEntries.Any() || ScoreEntries.All(se => scoreEntry.Score > se.Score))
             {
                 // Add new High Score
                 ScoreEntries.Add(scoreEntry);
+                HighScoresUpdated = true;
             }
 
             // Keep only the top 10 High Scores
