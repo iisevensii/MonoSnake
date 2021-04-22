3f48dd9abe89de88805253e7229586521a11f4b7
Nick Henry <iisevensii@gmail.com>
2021-04-21 22:43:16 -0500

    ScoreEntry class moved to its own class. Also removed unused setter.

--- Infrastructure/ScoreBoard.cs
+++ Infrastructure/ScoreBoard.cs
@@ -45,21 +45,4 @@ namespace MonoSnake.Infrastructure
             }
         }
     }
-
-    public class ScoreEntry
-    {
-        public string Name { get; set; }
-        public int Score { get; set; }
-
-        public ScoreEntry()
-        {
-            
-        }
-
-        public ScoreEntry(string name, int score)
-        {
-            Name = name;
-            Score = score;
-        }
-    }
 }
---
+++ Infrastructure/ScoreEntry.cs
@@ -0,0 +1,14 @@
+﻿namespace MonoSnake.Infrastructure
+{
+    public class ScoreEntry
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public ScoreEntry(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+    }
+}
