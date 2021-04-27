e6414c0a8e559bde03a80220fde8f35bb943ba3b
Nick Henry <iisevensii@gmail.com>
2021-04-26 22:21:04 -0500

    Switch from AnimatedSprite to custom blinking text (char) or rectangle for empty.

--- Infrastructure/TextEntry.cs
+++ Infrastructure/TextEntry.cs
@@ -22,6 +22,9 @@ namespace MonoSnake.Infrastructure
         private readonly ISprite _letterEntryAnimatedSprite;
         private Vector2 _position;
         private int _entryPosition = 0;
+        private float _currentCharBlinkTime = 0.375f;
+        private double _blinkTimeElapsed = 0d;
+        private bool _blinkOn = false;
 
         private Keys _currentChar = Keys.None;
         private string _inputString = String.Empty;
@@ -125,29 +128,55 @@ namespace MonoSnake.Infrastructure
 
         public void Update(GameTime gameTime)
         {
-            _letterEntryAnimatedSprite.Update(gameTime);
+            _blinkTimeElapsed += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_blinkTimeElapsed >= _currentCharBlinkTime)
+            {
+                _blinkOn = !_blinkOn;
+                _blinkTimeElapsed = 0d;
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
-            if(_currentChar == Keys.None)
-                _letterEntryAnimatedSprite.Draw(spriteBatch, _position, 0f);
+            Vector2 stringScale = _font.MeasureString(_currentChar.ToString());
+            if (_currentChar == Keys.None)
+            {
+                stringScale = _font.MeasureString("A");
+                if (_blinkOn)
+                {
+                    spriteBatch.Draw
+                    (
+                        _letterEntrySpriteFrame0.SpriteSheet,
+                        new Vector2(_position.X, _position.Y + stringScale.Y / 2 - 10),
+                        new Rectangle(_letterEntrySpriteFrame0.Left, _letterEntrySpriteFrame0.Top, _letterEntrySpriteFrame0.Width, _letterEntrySpriteFrame0.Height),
+                        _letterEntrySpriteFrame0.TintColor,
+                        0f,
+                        _letterEntrySpriteFrame0.Origin,
+                        _letterEntrySpriteFrame0.Scale,
+                        SpriteEffects.None,
+                        0f
+                    );
+                }
+            }
             else
             {
-                var stringScale = _font.MeasureString(_currentChar.ToString());
-
-                spriteBatch.DrawString
-                (
-                    _font,
-                    _currentChar.ToString(),
-                    new Vector2(_position.X, _position.Y - stringScale.Y /2),
-                    Color.White,
-                    0f,
-                    Vector2.Zero,
-                    1f,
-                    SpriteEffects.None,
-                    0f
-                );
+                stringScale = _font.MeasureString(_currentChar.ToString());
+                if (_blinkOn)
+                {
+                    spriteBatch.DrawString
+                    (
+                        _font,
+                        _currentChar.ToString(),
+                        new Vector2(_position.X, _position.Y - stringScale.Y / 2),
+                        Color.White,
+                        0f,
+                        Vector2.Zero,
+                        1f,
+                        SpriteEffects.None,
+                        0f
+                    );
+                }
             }
         }
     }
